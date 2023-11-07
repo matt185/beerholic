0c7f078d7c9989d32a62ecf69df0c1e457fa8040
Mattia <59755795+matt185@users.noreply.github.com>
2023-11-07 23:45:46 +0900

    add header create basic layout

---
+++ beerholic/src/components/Header.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import { BiMenuAltRight } from 'react-icons/bi'
+import { ISize } from '../types/generalTypes'
+
+interface HeaderProps {
+
+}
+
+export const Header: React.FC<HeaderProps> = ({ }) => {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
+    const [size, setSize] = useState<ISize>({
+        width: undefined,
+        height: undefined
+    })
+    useEffect(() => {
+        const handleResize: any = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            })
+            window.addEventListener('resize', handleResize)
+
+            return window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+    useEffect(() => {
+        if (size.width! > 768 && menuOpen) {
+            setMenuOpen(false)
+        }
+    }, [size.width, menuOpen])
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p)
+    }
+    return (
+        <header className="header">
+            <div className="header_content">
+                <div className="header_content_logo">
+                    <a href="/">BeerHolic</a>
+                </div>
+                <nav className={`${"header_content_nav"} ${menuOpen ? "isMenu" : ""}`}>
+                    <ul>
+                        <li>
+                            <a href="/">Beers</a>
+                        </li>
+                        <li>
+                            <a href="/favorite">Favorites</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="header_content_toggle">
+                    <BiMenuAltRight onClick={menuToggleHandler} />
+                </div>
+            </div>
+        </header>
+    )
+}
--- beerholic/src/components/Layout.tsx
+++ beerholic/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Footer } from './Footer'
+import { Header } from './Header'
 
 interface LayoutProps {
     children: React.ReactNode
@@ -8,6 +9,9 @@ interface LayoutProps {
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <>
+            <div className="layout_header">
+                <Header />
+            </div>
             <div className="layout_body">{children}</div>
             <div className="layout_footer">
                 <Footer />
